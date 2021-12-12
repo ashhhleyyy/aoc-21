613ac0d3f4f677c54beacb51268069f46fc2a421
Ash (ashisbored) <ash.b.7558@gmail.com>
2021-12-12 12:25:16 +0000

    Day 11: Dumbo Octopus

---
+++ dumbo-octopus/src/bin/part_2.rs
@@ -0,0 +1,83 @@
+use std::io::BufRead;
+
+use ansi_term::Style;
+
+fn main() {
+    let mut grid = [[0_u8; 10]; 10];
+    for (y, line) in std::io::stdin().lock().lines().map(Result::unwrap).enumerate() {
+        for (x, c) in line.chars().enumerate() {
+            let v = (c as u8) - ('0' as u8);
+            grid[y][x] = v;
+        }
+    }
+
+    let bold = Style::new().bold();
+
+    let mut i = 0;
+    loop {
+        i += 1;
+        let mut has_flashed_this_step = [[false; 10]; 10];
+        let mut flashes_this_round = 0;
+        
+        // 1. Increment light levels by 1
+        for x in 0..10 {
+            for y in 0..10 {
+                grid[y][x] += 1;
+            }
+        }
+        // 2. Any with level >9 flash. Increase nearby octopi by 1 energy level
+        let mut flash_queue = Vec::new();
+        for x in 0..10 {
+            for y in 0..10 {
+                if grid[y][x] > 9 {
+                    flash_queue.insert(0, (x, y));
+                }
+            }
+        }
+        while let Some((x, y)) = flash_queue.pop() {
+            if has_flashed_this_step[y][x] { continue; }
+            flashes_this_round += 1;
+            has_flashed_this_step[y][x] = true;
+            for xOffset in -1..=1 {
+                for yOffset in -1..=1 {
+                    if xOffset == 0 && yOffset == 0 { continue; }
+                    let (x, y) = ((x as isize) + xOffset, (y as isize) + yOffset);
+                    if x < 0 || x > 9 || y < 0 || y > 9 { continue; }
+                    grid[y as usize][x as usize] += 1;
+                    if grid[y as usize][x as usize] > 9 {
+                        flash_queue.push((x as usize, y as usize));
+                    }
+                }
+            }
+        }
+        // 3. Set level of all octopi that flashed to 0
+        for x in 0..10 {
+            for y in 0..10 {
+                if has_flashed_this_step[y][x] {
+                    grid[y][x] = 0;
+                }
+            }
+        }
+
+        if i % 10 == 0 || flashes_this_round == 100 {
+            println!("After step {}:", i);
+            for y in 0..10 {
+                for x in 0..10 {
+                    if has_flashed_this_step[y][x] {
+                        print!("{}", bold.paint(grid[y][x].to_string()));
+                    } else {
+                        print!("{}", grid[y][x]);
+                    }
+                }
+                println!()
+            }
+            println!()
+        }
+
+        if flashes_this_round == 100 {
+            break;
+        }
+    }
+
+    println!("All flash after round {}", i);
+}
